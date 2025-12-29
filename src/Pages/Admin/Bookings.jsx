8858831b05e7cef8840daabc144bebdd9320e159
Pramod Nichit <your-email@example.com>
2025-12-29 12:59:10 +0530

    s

--- src/Pages/Admin/Bookings.jsx
+++ src/Pages/Admin/Bookings.jsx
@@ -1,9 +1,6 @@
-
-
-
 import React, { useEffect, useState } from "react";
 
-const API_BASE = "https://backend-waghera.onrender.com/api";
+const API_BASE = "http://localhost:5000/api";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
